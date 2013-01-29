bbb95ddfb510eef9a80e42a2550adb0a12ef0af9
godvmxi <godvmxi@gmail.com>
2013-01-29 10:55:59 +0800

    first commit

---
+++ express/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Express::Application.config.secret_token = 'b2221629d7c5f71cf4edb7f50eb6a7f973f75072199ca34ae8b52545949908035a0fc876fb3a58c72d8bbfe09c6b9b95a9e764d2e79605ac31e9fac62da0fb17'
